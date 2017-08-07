52c5c9d8cefc0ae9def2d92f99ca634ca1cf96a3
Amit Dhamu <dev@amitd.co>
2017-08-07 21:52:20 +0100

    Fixed the loop

--- search.py
+++ search.py
@@ -31,35 +31,35 @@ mp3_path = None
 os.chdir(os.getcwd())
 
 
-def search_songs():
+def search_song(file):
     """Search for tracks and return api path."""
     search_url = base_url + "/search"
 
-    for file in glob.glob(mp3_path + "/**/*.mp3", recursive=True):
-        mp3_file = file
-        audiofile = eyed3.load(mp3_file)
-        artist = audiofile.tag.album_artist
-        track_name = audiofile.tag.title
-        track_length = math.ceil(audiofile.info.time_secs)
-        search_term = artist + ' ' + track_name
-        data = {'q': search_term}
-        request = requests.get(search_url, data=data, headers=headers)
-        response = request.json()
+    mp3_file = file
+    file_path = os.path.realpath(mp3_file)
+    audiofile = eyed3.load(mp3_file)
+    artist = audiofile.tag.album_artist
+    track_name = audiofile.tag.title
+    track_length = math.ceil(audiofile.info.time_secs)
+    search_term = artist + ' ' + track_name
+    data = {'q': search_term}
+    request = requests.get(search_url, data=data, headers=headers)
+    response = request.json()
 
-        print('Searching for ' + search_term)
-        if response['response']['hits']:
-            match = response['response']['hits'][0]
+    print('\tSearching for ' + search_term)
+    if response['response']['hits']:
+        match = response['response']['hits'][0]
 
-            return {
-                'artist': artist,
-                'track_name': track_name,
-                'track_length': track_length,
-                'mp3_path': mp3_path,
-                'api_path': match['result']['api_path']
-            }
-        else:
-            print('No results found')
-            return None
+        return {
+            'artist': artist,
+            'track_name': track_name,
+            'track_length': track_length,
+            'mp3_path': file_path,
+            'api_path': match['result']['api_path']
+        }
+    else:
+        print('No results found')
+        return None
 
 
 def lookup_song_info(artist, song_api_path, track_name, track_length, mp3_path):
@@ -71,7 +71,7 @@ def lookup_song_info(artist, song_api_path, track_name, track_length, mp3_path):
     producer = response['response']['song']['producer_artists'][0]
 
     if producer['name'] in target_producer:
-        print(producer['name'] + ' produced ' + track_name + '. Gonna add it to a playlist')
+        print('\t\t' + producer['name'] + ' produced ' + track_name + '. Gonna add it to a playlist')
         append_to_playlist(
             'Produced by ' + target_producer,
             mp3_path,
@@ -80,7 +80,7 @@ def lookup_song_info(artist, song_api_path, track_name, track_length, mp3_path):
             track_name
         )
     else:
-        print('Didn\'t match the producer we were searching for')
+        print('\t\tDidn\'t match the producer we were searching for')
 
 
 def append_to_playlist(playlist_name, mp3_path, track_length, artist, track_name):
@@ -159,12 +159,13 @@ if __name__ == "__main__":
         mp3_path = os.path.realpath(mp3_path)
 
     print('Gonna try and find songs produced by ' + target_producer)
-    song_info = search_songs()
-    if song_info is not None:
-        lookup_song_info(
-            song_info['artist'],
-            song_info['api_path'],
-            song_info['track_name'],
-            song_info['track_length'],
-            song_info['mp3_path']
-        )
+    for file in glob.glob(mp3_path + "/**/*.mp3", recursive=True):
+        song_info = search_song(file)
+        if song_info is not None:
+            lookup_song_info(
+                song_info['artist'],
+                song_info['api_path'],
+                song_info['track_name'],
+                song_info['track_length'],
+                song_info['mp3_path']
+            )
